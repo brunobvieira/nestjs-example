69b33f2b3d17fbade7f5804d8f197c6e84d4324b
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2022-02-10 17:49:35 -0300

    create users table

---
+++ src/database/migrations/1644515793588-create_users_table.ts
@@ -0,0 +1,58 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class createUsersTable1644515793588 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.createTable(
+      new Table({
+        name: 'users',
+        schema: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isUnique: true,
+            generationStrategy: 'uuid',
+            default: `uuid_generate_v4()`,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            length: '100',
+            isUnique: true,
+            isNullable: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
+            length: '100',
+            isNullable: false,
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()',
+            isNullable: false,
+          },
+          { name: 'deletedAt', type: 'timestamp', isNullable: true },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable(new Table({ name: 'users', schema: 'users' }));
+  }
+}
