cfa519d101c92db1df77c0322ce8b229ff946846
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2022-02-10 19:36:18 -0300

    add swagger props

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,33 +1,33 @@
-import {
-  Column,
-  CreateDateColumn,
-  DeleteDateColumn,
-  Entity,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
 import { CreateUserDto } from '../modules/user/dtos/createUser.dto';
 
 @Entity({ schema: 'users', name: 'users' })
 export class User {
+  @ApiProperty()
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ApiProperty()
   @Column()
   name: string;
 
+  @ApiProperty()
   @Column()
   email: string;
 
   @Column({ select: false })
   password: string;
 
+  @ApiProperty()
   @CreateDateColumn()
   createdAt: Date;
 
+  @ApiProperty()
   @UpdateDateColumn()
   updatedAt: Date;
 
+  @ApiProperty()
   @DeleteDateColumn()
   deletedAt: Date;
 }
--- src/modules/user/dtos/createUser.dto.ts
+++ src/modules/user/dtos/createUser.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import {
   IsNotEmpty,
   MaxLength,
@@ -7,16 +8,19 @@ import {
 } from 'class-validator';
 
 export class CreateUserDto {
+  @ApiProperty()
   @IsNotEmpty()
   @MaxLength(100)
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MaxLength(100)
   @IsEmail()
   email: string;
 
+  @ApiProperty()
   @IsNotEmpty()
   @MinLength(8)
   @IsString()
