1e6fb5e2f553b79b1c8a35c298fae5e198c1854b
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2022-02-10 19:35:39 -0300

    add swagger doc

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
+  // SWAGGER CONFIG
+  const config = new DocumentBuilder()
+    .setTitle('Test Project')
+    .setDescription('A test project api')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
