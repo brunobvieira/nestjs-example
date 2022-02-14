e059be859d8183438fcb8460e2a55bda305c1ade
Bruno Beserra VIeira <brunobeserravieira@gmail.com>
2022-02-14 10:05:51 -0300

    validateUser Method

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { compareSync } from 'bcrypt';
+import { UserService } from '../user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.userService.findOneWithPassword({ email });
+
+    if (user && compareSync(password, user.password)) {
+      delete user.password;
+      return user;
+    }
+
+    return null;
+  }
+}
